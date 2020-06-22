e1d720cdda433453b04d36905f9ac4d063266b81
Matheus Glauber Rodrigues Jordão <glauber.jordao1995@gmail.com>
2020-06-22 20:57:39 -0300

    Criando uma imagem para o projeto.

---
+++ README.md
@@ -0,0 +1,4 @@
+# disciplina-pp
+
+Crie a base de dados com o nome loja.
+Insira o comando: `npx sequelize db:migrate` para subir as tabelas.
---
+++ frontend/src/pages/SignUp/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import { signUpRequest } from '../../store/modules/auth/actions';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um email válido')
+    .required('O Email é obrigatório'),
+  password: Yup.string()
+    .min(6, 'A senha precisa de ter 6 caracteres no mínimo')
+    .required('A Senha é obrigatória'),
+});
+
+export default function SignUp() {
+  const dispatch = useDispatch();
+
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
+  }
+
+  return (
+    <>
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </Form>
+    </>
+  );
+}
